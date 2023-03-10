eb4704cb14336da580dced148428c20fbfdc5959
Hezx13 <skzalupa@gmail.com>
2023-03-10 10:32:12 +0100

    - button hover transitions - Drag&Drop Events working

--- quiz/Form1.Designer.cs
+++ quiz/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace quiz
 {
-    partial class Form1
+    partial class Quiz
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,18 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            button3 = new Button();
+            question = new Label();
             SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(113, 214, 142);
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(113, 214, 142);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(71, 470);
+            button1.Name = "button1";
+            button1.Size = new Size(210, 52);
+            button1.TabIndex = 0;
+            button1.Text = "Answer";
+            button1.UseVisualStyleBackColor = false;
+            button1.MouseLeave += button1_MouseLeave;
+            button1.MouseHover += button1_MouseHover;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(113, 214, 142);
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(113, 214, 142);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(384, 470);
+            button3.Name = "button3";
+            button3.Size = new Size(210, 52);
+            button3.TabIndex = 2;
+            button3.Text = "Show Answer";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            button3.MouseLeave += button3_MouseLeave;
+            button3.MouseHover += button3_MouseHover;
+            // 
+            // question
+            // 
+            question.AutoSize = true;
+            question.BackColor = Color.Transparent;
+            question.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            question.ForeColor = Color.FromArgb(187, 162, 247);
+            question.Location = new Point(71, 44);
+            question.Name = "question";
+            question.Size = new Size(141, 41);
+            question.TabIndex = 3;
+            question.Text = "question";
+            question.Click += question_Click;
+            // 
+            // Quiz
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1038, 609);
-            Name = "Form1";
+            BackColor = Color.FromArgb(16, 40, 48);
+            ClientSize = new Size(665, 586);
+            Controls.Add(question);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Name = "Quiz";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button3;
+        private Label question;
     }
 }
--- quiz/Form1.cs
+++ quiz/Form1.cs
@@ -1,10 +1,74 @@
+using System.Drawing;
+using Transitions;
+
 namespace quiz
 {
-    public partial class Form1 : Form
+    public partial class Quiz : Form
     {
-        public Form1()
+        public Quiz()
         {
             InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Quiz_DragEnter);
+            this.DragDrop += new DragEventHandler(Quiz_DragDrop);
+        }
+
+        void Quiz_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Quiz_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files) question.Text = file;
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            var t = new Transition(new TransitionType_EaseInEaseOut(300));
+            t.add(button1, "BackColor", Color.FromArgb(255, 187, 162, 247));
+            t.add(button1, "ForeColor", Color.Black);
+            t.run();
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            var t = new Transition(new TransitionType_EaseInEaseOut(300));
+            t.add(button1, "BackColor", Color.FromArgb(255, 113, 214, 142));
+            t.add(button1, "ForeColor", Color.White);
+            t.run();
+            button1.BackColor = Color.FromArgb(1, 113, 214, 142);
+            button1.ForeColor = Color.White;
+
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            var t = new Transition(new TransitionType_EaseInEaseOut(300));
+            t.add(button3, "BackColor", Color.FromArgb(255, 187, 162, 247));
+            t.add(button3, "ForeColor", Color.Black);
+            t.run();
+
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            var t = new Transition(new TransitionType_EaseInEaseOut(300));
+            t.add(button3, "BackColor", Color.FromArgb(255, 113, 214, 142));
+            t.add(button3, "ForeColor", Color.White);
+            t.run();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void question_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
